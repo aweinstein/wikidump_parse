66976b922c8cbc022a99d536c9a7fcac33c6d60e
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2012-06-01 20:35:55 -0600

    Add query option. Still need to handle articles across file boundaries.

--- block_parse.py
+++ block_parse.py
@@ -15,6 +15,10 @@ import sys
 import unicodedata
 import xml.sax
 
+xml_header = '''
+<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.6/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.6/ http://www.mediawiki.org/xml/export-0.6.xsd" version="0.6" xml:lang="en">
+'''
+
 def to_ascii(s):
     try:
         s_ascii = unicodedata.normalize('NFKD', s).encode('ascii','ignore')
@@ -24,12 +28,18 @@ def to_ascii(s):
 
 
 class WikiDumpHandler(xml.sax.ContentHandler):
-    def __init__(self, db):
+    def __init__(self, db, parse='all', query=False):
+        '''
+        parse: 'all' parse all the articles
+               'query'  
+        '''
         xml.sax.ContentHandler.__init__(self)
         self.title_flag = False
         self.text_flag = False
         self.n = 0
-        self.db = db
+        self.db = db 
+        self.parse = parse
+        self.query = query
         
     def startElement(self, name, attrs):
         if name == 'title':
@@ -56,29 +66,57 @@ class WikiDumpHandler(xml.sax.ContentHandler):
 
             if title is None or txt is None:
                 txt = ' '
-                
-            self.db[title] = txt
+
+            if self.parse == 'all':
+                self.db[title] = self.f_name
+                if self.n % 1000 == 0:
+                    print 'Adding article', self.n
+            else:
+                if title == self.query:
+                    self.answer = txt
             
             self.n += 1
 
-            if self.n % 1000 == 0:
-                print 'Adding article', self.n
+    
+def query(query, db):
 
-                 
+    try:
+        fn = os.path.join('split', db[query])
+    except KeyError:
+        return None
+    
+    dump = ''.join(bz2.BZ2File(fn).readlines())
+
+    parser = xml.sax.make_parser()
+    sax_handler = WikiDumpHandler(articles_db, 'query', query)
+    parser.setContentHandler(sax_handler)
+    parser.feed(xml_header)
+    parser.feed(dump[dump.index('<page>'):])
+    
+    return sax_handler.answer
+
 if __name__ == '__main__':
+    fn_db = 'articles_block'
+    articles_db = dbm.open(fn_db, 'r')
+    txt = query('A', articles_db)
+    print txt
+
+
+if __name__ == '__main__x':
     fn_db = 'articles_block'
     articles_db = dbm.open(fn_db, 'n')
 
     split_dir = 'split'
-    blocks = (bz2.BZ2File(os.path.join(split_dir, f))
+    
+    blocks = ((f, bz2.BZ2File(os.path.join(split_dir, f))) 
               for f in sorted(os.listdir(split_dir)))
 
-
     parser = xml.sax.make_parser()
-    parser.setContentHandler(WikiDumpHandler(articles_db))
-    for block in blocks:
+    sax_handler = WikiDumpHandler(articles_db)
+    parser.setContentHandler(sax_handler)
+    for f_name, block in blocks:
+        sax_handler.f_name = f_name
         parser.feed(''.join(block.readlines()))
-
 
     print '%d articles added to %s' % (len(articles_db), fn_db)
     articles_db.close()
